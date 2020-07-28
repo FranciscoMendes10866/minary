9b47fa79fff90399baf27cefb0e7afb96010f58a
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-07-28 17:02:04 +0100

    home page design done and post component

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="max-w-2xl py-8">
+    <h1
+      class="mb-2 text-orange-500 text-sm font-semibold uppercase font-normal tracking-wide"
+    >Tech talk</h1>
+    <router-link
+      class="text-blue-900 font-bold text-2xl"
+      to="/"
+    >How to Position Your Idea With User Research</router-link>
+    <p
+      class="mt-2 text-gray-700 text-md font-normal tracking-wide"
+    >Positioning your product is not easy or obvious. Explore user research methods and tools that help you guide product development and customer satisfaction.</p>
+    <p class="mt-4 text-gray-500">Written by: francisco</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="home">
-    <div class="container mx-auto">
+    <!-- Hero Landing -->
+    <section>
       <div class="grid grid-cols-1">
-        <div class="text-center">
-          <h1 class="font-semibold tracking-wide text-4xl mt-20">Hello world</h1>
+        <div class="bg-indigo-700 py-24 text-center">
+          <h1 class="text-white font-bold text-4xl">BLOG</h1>
+          <p class="text-white font-normal text-lg mt-2">Let's share some knowledge.</p>
+          <input
+            class="mt-8 rounded-sm px-64 py-4 text-center text-lg"
+            type="text"
+            placeholder="Search an article"
+          />
         </div>
       </div>
-    </div>
+    </section>
+    <!-- Posts Section -->
+    <section>
+      <div class="container mx-auto">
+        <div class="flex flex-auto justify-center">
+          <Posts />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  components: {
+    Posts: () => import('../components/Post')
+  }
+}
 </script>
